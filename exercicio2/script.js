7db15399f41bd43fa5af5608fdc2075946f3db16
Ikebonamin <bonamin.ike@gmail.com>
2022-08-30 08:10:15 -0300

    execução exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+"strict";
+
+let pergunta1 = prompt("Você está com frio?");
+let pergunta2 = prompt("Você possui um carro?");
+let pergunta3 = prompt("Você vai viajar no feriado?");
+// console.log(pergunta1, pergunta2, pergunta3);
+
+let resposta1 = pergunta1;
+let resposta2 = pergunta1;
+let resposta3 = pergunta1;
+console.log(resposta1, resposta2, resposta3);
+
+console.log(`Você está com frio? ${resposta1}`);
+console.log(`possui um carro? ${resposta2}`);
+console.log(`Você vai viajar no feriado? ${resposta3}`);
